8554ebad0c358c2cc7e6bc04366ed1b74cf81e8a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-04-24 17:06:11 +0200

    Upgrade implementation finder to Jekyll > 3.0

--- _plugins/projectlist.rb
+++ _plugins/projectlist.rb
@@ -31,7 +31,7 @@ module Jekyll
     #
     # Returns the HTML formatted String.
     def markdownify(input)
-      converter = @site.getConverterImpl(Jekyll::Converters::Markdown)
+      converter = @site.find_converter_instance(Jekyll::Converters::Markdown)
       converter.convert(input)
     end
   end
